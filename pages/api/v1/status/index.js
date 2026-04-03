c540a1ae112a530d064aaea18cddadfce5bcde19
Cássia <cascrisbern@gmail.com>
2026-04-03 16:35:12 +0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,27 @@
 import database from "infra/database.js"
 
 export default async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;')
 
-  console.log(result.rows)
+  const databaseName = process.env.POSTGRES_DB
 
+  const updatedAt = new Date().toISOString()
+  const version = await database.query("SHOW server_version;")
+  const maxConnections = await database.query("SHOW max_connections;")
+  const openedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  })
+  
   response.status(200).json({
-    chave: "valor",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: version.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        opened_connections: openedConnections.rows[0].count
+      },
+        
+      }
+
   });
 }
--- tests/api/v1/status/get.test.js
+++ tests/api/v1/status/get.test.js
@@ -1,5 +1,47 @@
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
+describe("GET to /api/v1/status", () => {
 
-  expect(response.status).toBe(200);
+  let response
+  let responseBody
+
+  beforeEach(async () => {
+    response = await fetch("http://localhost:3000/api/v1/status");
+    responseBody = await response.json()
+  })
+  
+  it("should return status 200", ()=> {
+    expect(response.status).toBe(200);
+  })
+
+  it("should return a valid date", () => {
+    expect(responseBody.updated_at).toBeDefined()
+    const parseUpdatedAt = new Date(responseBody.updated_at).toISOString()
+    expect(responseBody.updated_at).toEqual(parseUpdatedAt)
+  })
+
+  it("should return version 16.0 of Postgres", () => {
+    expect(responseBody.dependencies.database.version).toBe("16.0")
+  })
+
+  it("should return max_connections equal to 100", () => {
+    expect(responseBody.dependencies.database.max_connections).toEqual(100)
+  })
+
+  it("should return opened_connections equal to 1", () => {
+    expect(responseBody.dependencies.database.opened_connections).toEqual(1)
+  })
+  
+  it("should return correct fields", () => {
+    expect(responseBody).toHaveProperty("updated_at")
+    expect(responseBody.dependencies.database).toHaveProperty("version")
+    expect(responseBody.dependencies.database).toHaveProperty("max_connections")
+    expect(responseBody.dependencies.database).toHaveProperty("opened_connections")
+  })
+
+  it("should return correct types", () => {
+    expect(typeof responseBody.dependencies.database.version).toBe("string")
+    expect(typeof responseBody.dependencies.database.max_connections).toBe("number")
+    expect(typeof responseBody.dependencies.database.opened_connections).toBe("number") 
+  })
 });
+
+
